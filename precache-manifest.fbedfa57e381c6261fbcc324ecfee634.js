ff5dcfc89944cba0e6f7266b9504d63e929c917f
Billie <mail@billie.co>
2020-10-29 12:41:19 +0100

    SITE

---
+++ precache-manifest.fbedfa57e381c6261fbcc324ecfee634.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47524fb80ce755f3a95a0eaaca613458",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6300cba5ed72f05ba874",
+    "url": "/static/css/main.9f0fa432.chunk.css"
+  },
+  {
+    "revision": "a89f6cbd2f5d4bb71294",
+    "url": "/static/js/2.afb96dc5.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.afb96dc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6300cba5ed72f05ba874",
+    "url": "/static/js/main.6675f579.chunk.js"
+  },
+  {
+    "revision": "e9b78b3a42733e9071bb",
+    "url": "/static/js/runtime-main.17c7128f.js"
+  },
+  {
+    "revision": "daeaf731dec30f7b361b6cffbbbc307f",
+    "url": "/static/media/avatar2.daeaf731.png"
+  }
+]);
