623cef3fe0c5f0283399187b06ce5ff731c9f433
Billie Chan <mail@billie.co>
2023-08-27 10:25:10 +0200

    Push to GitHub

---
+++ precache-manifest.4470dea802d4cd0efc002946c7b54f03.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92bdfe81a8687c4287ac05753c8109a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "770baeb634f34c0442f6",
+    "url": "/static/css/main.e8a19fbb.chunk.css"
+  },
+  {
+    "revision": "9d48ec2e7906dfb83728",
+    "url": "/static/js/2.9ceaef05.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.9ceaef05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "770baeb634f34c0442f6",
+    "url": "/static/js/main.933e212c.chunk.js"
+  },
+  {
+    "revision": "e9b78b3a42733e9071bb",
+    "url": "/static/js/runtime-main.17c7128f.js"
+  },
+  {
+    "revision": "d25ee54fee0c73f4f16d1edf0dfcae3b",
+    "url": "/static/media/Resume.d25ee54f.pdf"
+  },
+  {
+    "revision": "b5612632ac8bd8efc5ad3786ce705b2f",
+    "url": "/static/media/avatar2.b5612632.png"
+  },
+  {
+    "revision": "a2d7ed1d8b7b92aa2bb452ec1eb74aa8",
+    "url": "/static/media/blog.a2d7ed1d.svg"
+  },
+  {
+    "revision": "d896ddbf4544b7eeb61f6f362592d88b",
+    "url": "/static/media/budget.d896ddbf.svg"
+  },
+  {
+    "revision": "2e46301492dbcb0923c9d77692bf56cb",
+    "url": "/static/media/capex.2e463014.svg"
+  },
+  {
+    "revision": "7ab08c892804aeb20ec1bd4c73275514",
+    "url": "/static/media/china.7ab08c89.svg"
+  },
+  {
+    "revision": "e5c92ef60cb31b4e5332ef37d53b8054",
+    "url": "/static/media/cv.e5c92ef6.svg"
+  },
+  {
+    "revision": "0be1c7362ff00377105eacfa597234ee",
+    "url": "/static/media/documentation.0be1c736.svg"
+  },
+  {
+    "revision": "cf78973c4b6ba97796aa2dd1bc5b5bea",
+    "url": "/static/media/email.cf78973c.svg"
+  },
+  {
+    "revision": "d91d1233e1a2e6a22e8324129e503912",
+    "url": "/static/media/excel.d91d1233.svg"
+  },
+  {
+    "revision": "69058c8288dbee636178235c78874a48",
+    "url": "/static/media/germany.69058c82.svg"
+  },
+  {
+    "revision": "dfdcfe62a4ffe997bc0ff3a0707dce51",
+    "url": "/static/media/input.dfdcfe62.svg"
+  },
+  {
+    "revision": "04cc6f8c89cab40affeae859743702c4",
+    "url": "/static/media/linkedin.04cc6f8c.svg"
+  },
+  {
+    "revision": "8b31cbc41f0ba607d9b220120ae4f05d",
+    "url": "/static/media/phone.8b31cbc4.svg"
+  },
+  {
+    "revision": "fa5c461e86b5726416ab5e412c8d324b",
+    "url": "/static/media/powerquery.fa5c461e.svg"
+  },
+  {
+    "revision": "102ec777c20ed0f0b4951860de16dda9",
+    "url": "/static/media/predict.102ec777.svg"
+  },
+  {
+    "revision": "fcbf0fc3461fba24696f90d42ada6802",
+    "url": "/static/media/r.fcbf0fc3.svg"
+  },
+  {
+    "revision": "7c3ece2b6bf4f22eea4e77c60374764d",
+    "url": "/static/media/rpa.7c3ece2b.svg"
+  },
+  {
+    "revision": "073a9648ef93fa5408e38094c741e2cc",
+    "url": "/static/media/sap.073a9648.svg"
+  },
+  {
+    "revision": "fc7bfaebd51064dcddbfd29e5bfcad5f",
+    "url": "/static/media/skype.fc7bfaeb.svg"
+  },
+  {
+    "revision": "c57ae131ef5e2d371f5a8502d6d6a19a",
+    "url": "/static/media/sql.c57ae131.svg"
+  },
+  {
+    "revision": "fafbb619f27917cce2fbc3faa74a14aa",
+    "url": "/static/media/startup.fafbb619.svg"
+  },
+  {
+    "revision": "148cae2ec843ecef83caf4606d9d3c88",
+    "url": "/static/media/tableau.148cae2e.svg"
+  },
+  {
+    "revision": "a03794ee3b1647bd7ec28b6e4a7d1d94",
+    "url": "/static/media/training.a03794ee.svg"
+  },
+  {
+    "revision": "defecbdbe62d55b8e69e5af106c5a930",
+    "url": "/static/media/translate.defecbdb.svg"
+  },
+  {
+    "revision": "8b588113a2cd0c8de94931a94688ef9a",
+    "url": "/static/media/united_states.8b588113.svg"
+  },
+  {
+    "revision": "fd95e7dc346966e20ab1c5e812d1b7c0",
+    "url": "/static/media/variance.fd95e7dc.svg"
+  },
+  {
+    "revision": "e2efe92e2bf087ed8d2c5424725f4dea",
+    "url": "/static/media/wechat.e2efe92e.svg"
+  },
+  {
+    "revision": "c94b9ae7efee2c036b384c6f12a0119a",
+    "url": "/static/media/whatsapp.c94b9ae7.svg"
+  }
+]);
